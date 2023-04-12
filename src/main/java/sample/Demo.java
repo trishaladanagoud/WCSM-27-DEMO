8669305598cffcdaa9a7ff4159b18d056a88cd68
shree <shree@sony-PC>
2023-04-12 09:50:10 -0700

    demo is commited now

--- src/main/java/sample/Demo.java
+++ src/main/java/sample/Demo.java
@@ -3,7 +3,11 @@ package sample;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("shree ganesh");   
+		System.out.println("shree ganesh");  
+		for(int i=0;i<=5;i++)
+		{
+			System.out.println("krishna");
+		}
 
 	}
 
